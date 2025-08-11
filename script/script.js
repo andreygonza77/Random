5b6df37bd8f54993f8a349b644745b35088b1b22
gonzales7andrey <gonzalescano.andrey@itismeucci.com>
2025-08-11 16:23:44 +0200

    new update

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector(".btn");
 const passwordTxt = document.querySelector(".passwordTxt");
-const array = [
+const max = 16;
+const characters = [
   "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
   "a", "s", "d", "f", "g", "h", "j", "k", "l",
   "z", "x", "c", "v", "b", "n", "m",
@@ -13,12 +14,17 @@ const array = [
 
   "!", "?", "%", "/", "£", "€", "&", "(", ")", "=", "'", "+", "-", "*", ",", ".", ";", ":", "_", "@", "#", "$", "^"];
 
+function getSecureIndex(n){
+    const array = new Uint32Array(1);
+    window.crypto.getRandomValues(array);
+    return array[0] % n;
+}
+
 function getPassword(){
     let password = "";
-    for(let i = 0; i < 16; i++){
-        let n = Math.floor(Math.random() * array.length);    
-        console.log(array[n]);
-        password += array[n];
+    for(let i = 0; i < max; i++){
+        let index = getSecureIndex(characters.length) ;
+        password += characters[index];
     }
     return password;
 }
@@ -29,13 +35,33 @@ function start(){
 
 btn.addEventListener("click", start);
 
+/*
+Using Math.Random()
+
+function getPassword(){
+    let password = "";
+    for(let i = 0; i < 16; i++){
+        let n = Math.floor(Math.random() * characters.length);    
+        console.log(characters[n]);
+        password += characters[n];
+    }
+    return password;
+}
+
+function start(){
+    passwordTxt.innerHTML = getPassword();
+}
+
+btn.addEventListener("click", start);
+*/
+
 
 /*
+
 function printArray(){
-    for(let i = 0; i < array.length; i++){
-        console.log(array[i]);
+    for(let i = 0; i < characters.length; i++){
+        console.log(characters[i]);
     }
 }
 
-document.addEventListener("DOMContentLoaded", printArray);
-*/
+document.addEventListener("DOMContentLoaded", printArray);*/
